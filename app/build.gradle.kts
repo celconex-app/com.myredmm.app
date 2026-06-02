3c4af3c2eac939f49b5733af8077d2ae1f057f4d
🪶Arturo  ochoa 🦅 Orange 🍊 <209074126+arturoochoa0606@users.noreply.github.com>
2026-06-01 23:46:46 -0600

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+android {
+
+    namespace = "com.myredmm.app"
+
+    defaultConfig {
+        applicationId = "com.myredmm.app"
+
+        minSdk = 26
+        targetSdk = 33
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
